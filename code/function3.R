cfa413b109d4e9aadd9ed098a8faa1c34aa0e331
PiotrLeTrong <ntl28@georgetown.edu>
2017-04-25 15:20:58 -0400

    Finished initial OLS estimation of Function
    There is a lot left to fix.

--- code/function3.R
+++ code/function3.R
@@ -1,7 +1,16 @@
 rm(list = ls())
-load("/Users/piotr/Dropbox/School/Spring 2017/670 Data Sci/DataSciProject/data/1.raw/footballdata.RData")
+load("/Users/piotr/Dropbox/School/Spring 2017/670 Data Sci/regressr-library-ghl/data/1.raw/footballdata.RData")
+
+for(i in 1:ncol(cfb.scoring)){
+  if(class(cfb.scoring[[i]]) == "factor"){
+    cfb.scoring[[i]] <- as.character(cfb.scoring[[i]])
+  }
+  if(grepl("[0-9]", cfb.scoring[[i]])==TRUE | cfb.scoring[[i]]==""){
+    cfb.scoring[[i]] <- as.numeric(cfb.scoring[[i]])
+  }
+}
 # test test test
-functiontres <- function(df,
+interpreter <- function(df,
                         modelType, 
                         dependentVar, 
                         independentVar, 
@@ -16,10 +25,8 @@ functiontres <- function(df,
       df[[i]] <- as.numeric(df[[i]])
     }
   }
-  modelType <- deparse(substitute(modelType))
   modelType <- tolower(modelType)
   if (logDepen == T){
-    dependentVar <- deparse(substitute(dependentVar))
     df[,dependentVar] <- log(df[,dependentVar])
     df[,dependentVar]
   }
@@ -30,8 +37,27 @@ functiontres <- function(df,
       df[,name]
     }
   }
-  # if(modelType == "ols"){
-  #   
+  modelSpec <- paste(dependentVar, "~", paste(independentVar, collapse = " + "))
+  modelSpec
+  if(modelType == "ols"){
+    output <- lm(noquote(modelSpec), df)
+    summary(output)
+    varNames <-   row.names(coef(summary(output)))
+    coefficients <- as.numeric(coef(summary(output))[,1])
+    error <- as.numeric(coef(summary(output))[,2])
+    tVal <- as.numeric(coef(summary(output))[,3])
+    pVal <- as.numeric(coef(summary(output))[,4])
+    for(i in 2:length(varNames)){
+      if(pVal[i] < 1.95){
+        writeLines(paste("In this regression the independent variable", varNames[i],
+                         " has a statistically significant impact on the dependent variable",
+                         "a 1 unit increase in the independent variable causes a ", sprintf("%.3f",coefficients[i]),
+                         "increase in the dependent variable. \n"))
+      } else {
+        writeLines(paste("In this regression the independent variable", varNames[i],
+                         " does not have  a statistically significant relationship with the dependent variable"))
+      }
+    }
   # } else if (modelType == "probit") {
   #   
   # } else if (modelType == "logit") {
@@ -42,19 +68,25 @@ functiontres <- function(df,
   #   
   # } else if (modelType == "multinominal logit") {
   #   
-  # } else{
-  #   writeLines("\n Unsupported model type, please try one of the followwing:
-  #              \n - OLS
-  #              \n - probit
-  #              \n - logit
-  #              \n - tobit
-  #              \n - multinominal probit
-  #              \n - multinominal logit")
-  # }
-  print(df)
+  } else {
+    writeLines("\n Unsupported model type, please try one of the followwing:
+               \n - OLS
+               \n - probit
+               \n - logit
+               \n - tobit
+               \n - multinominal probit
+               \n - multinominal logit")
+  }
 }
-cfb.scoring.tiny <- cfb.scoring[,1:4]
 
-test(cfb.scoring, offensive.G)
+test <- function(df,
+                        modelType, 
+                        dependentVar, 
+                        independentVar){
+  #dependentVar <- deparse(substitute(dependentVar))
+  #independentVar <- deparse(substitute(independentVar))
+  
 
-functiontres(ols, offensive.G, offensive.TD, cfb.scoring.tiny, T)
+}
+
+interpreter(df = cfb.scoring, modelType = "ols", dependentVar = "offensive.TD", independentVar =  c("defensive.TD", "defensive.FG"))
